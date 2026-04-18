06332d28307b15f17673050794d3b9678c0a47c0
sandyberko <104715381+sandyberko@users.noreply.github.com>
2026-04-18 12:00:21 +0300

    inline common.sh
    Added error handling and a print function for colored output.

--- init-apt.sh
+++ init-apt.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-source ./common.sh
+set -euo pipefail
+
+print() {
+    local BLUE_BOLD="\033[1;34m"
+    local RESET="\033[0m"
+    echo -e "🔷 ${BLUE_BOLD}$*${RESET}"
+}
 
 cd $HOME
 
