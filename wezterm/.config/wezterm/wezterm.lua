10de38d50bbc44c335d2c6b83e4b8204341623a4
Sandy Berkowitz <sandyberko@outlook.com>
2026-07-24 09:49:15 +0300

    noctalia

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- config.color_scheme = 'Catppuccin Macchiato'
-config.color_scheme = 'dank-theme'
+config.color_scheme = "Noctalia"
 config.font = wezterm.font 'Maple Mono'
 config.window_background_opacity = 0.8
 config.hide_tab_bar_if_only_one_tab = true
